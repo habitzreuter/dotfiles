05e2b7fe8eef3433c5f3c218ed0c078564fd4488
Marco Antônio Habitzreuter <habitzreuter@mailbox.org>
2019-05-20 21:47:28 -0300

    Add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,11 @@
+set fish_greeting
+
+set -gx EDITOR vim
+set -gx LANG en_US.UTF-8
+set -gx PATH ~/bin/(hostname) ~/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin
+
+
+function fish_prompt
+    printf '%s' (prompt_pwd)
+    printf ' > '
+end
