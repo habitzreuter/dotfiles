21a0df46bd237528fddf7fdca15b20ecdfa6cdb1
Marco Antônio Habitzreuter <habitzreuter@mailbox.org>
2019-05-23 16:04:10 -0300

    Update fish $PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ set fish_greeting
 
 set -gx EDITOR vim
 set -gx LANG en_US.UTF-8
-set -gx PATH ~/bin/(hostname) ~/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin
+set -gx PATH ~/bin/(hostname) ~/bin ~/.local/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin
 
 
 function fish_prompt
