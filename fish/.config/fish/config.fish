e529591478e70b76546141b7de532638fe33bb24
Marco Antônio Habitzreuter <mahabitzreuter@gmail.com>
2021-06-05 10:26:46 -0300

    Removed unused config from fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,17 +4,3 @@ set -gx EDITOR vim
 set -gx LANG en_US.UTF-8
 set -gx PATH ~/bin/(hostname) ~/bin ~/.local/bin /usr/local/bin /usr/bin /bin /sbin /usr/sbin
 
-
-function fish_prompt
-    printf '%s' (prompt_pwd)
-    printf ' > '
-end
-
-# Enable fzf keybindings
-function fish_user_key_bindings
-	fzf_key_bindings
-end
-
-if test (tty) = /dev/tty1
-	eval sway
-end
