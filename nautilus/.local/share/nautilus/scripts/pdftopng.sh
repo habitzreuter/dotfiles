d5126bb54705ad1841c0bd0cab4962f6a25ce54b
Marco Habitzreuter <habitzreuter@ufrgs.br>
2023-12-05 19:59:21 -0300

    Fix Nautilus PNG script

--- nautilus/.local/share/nautilus/scripts/pdftopng.sh
+++ nautilus/.local/share/nautilus/scripts/pdftopng.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 IFS=$'\n'
+# See https://stackoverflow.com/a/53180170 for permission
 for picture in $NAUTILUS_SCRIPT_SELECTED_FILE_PATHS; do
 	png_file=`echo "$picture" | sed 's/\.\w*$/.png/'`
-	convert -density 300 $picture -quality 90 $png_file
+	convert -background white -alpha remove -alpha off -density 300 $picture -quality 90 $png_file
 done
